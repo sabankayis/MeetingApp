529cc8dfdb5c18f9b90b3a2c23c0d1444482c26f
sabankayis <sbnkys1@gmail.com>
2024-11-03 00:05:29 +0300

    Project is finished!

---
+++ Controllers/MeetingController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using MeetingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MeetingApp.Controllers
+{
+    public class MeetingController : Controller
+    {
+        
+        [HttpGet]
+        public IActionResult Apply(){
+            return View();
+        }
+          [HttpPost]
+        public IActionResult Apply(UserInfo model){
+            if (ModelState.IsValid)
+            {
+            Repository.CreateUser(model);
+            ViewBag.UserCount = Repository.Users.Where(info=>info.WillAttend == true).Count();
+            return View("Thanks",model); 
+            } else {
+                return View(model);
+            }
+
+        }
+        [HttpGet]
+        public IActionResult List(){
+            return View(Repository.Users);
+        }
+        public IActionResult Details(int id){
+            return View(Repository.GetById(id));
+        }
+
+
+    }
+}
